bf0f83513c38f975cb38ae8398f55e29233a1ef9
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-09-01 19:43:00 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,7 +2,7 @@ class Puppy
 
   attr_accessor :name, :breed, :months_old
 
-  def initializate(name, breed, months_old)
+  def initializate(name, breed, age)
     @name = name
     @breed = breed
     @age
